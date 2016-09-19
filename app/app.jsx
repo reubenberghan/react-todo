9db87b6252d9e7630e6ac993b0a06280cd5e0791
Reuben Berghan <reuben.berghan@gmail.com>
2016-09-20 07:20:22 +1000

    Init commit

---
+++ app/app.jsx
@@ -0,0 +1,21 @@
+'use strict';
+
+// external modules
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+// using es6s destructuring feature to pull out all the required variables from the `react-route` module
+var { Route, Router, IndexRoute, hashHistory } = require('react-router');
+
+// load foundation
+$(document).foundation();
+
+// custom app css
+require('style!css!sass!applicationStyles');
+
+// the `IndexRoute` component lets `react` know which component is our default (index) route to render
+// we then nest further `Route` components to define the components we want rendered at which route
+ReactDOM.render(
+    <p>Boilerplate 3 Project</p>,
+    document.getElementById('app')
+);
