72e150b949b738889cc56ef109e3e225742bb8db
Reuben Berghan <reuben.berghan@gmail.com>
2016-09-22 07:16:00 +1000

    Add tests for Todo components

---
+++ app/tests/components/TodoList.test.jsx
@@ -0,0 +1,21 @@
+'use strict';
+
+// all the modules required to run the tests on the components
+var React = require('react');
+var ReactDOM = require('react-dom');
+var expect = require('expect');
+var $ = require('jQuery');
+// this is a special module required for react testing as we need to be able to 'render' the component to test its functions
+// and this module provides the utilities to do so
+var TestUtils = require('react-addons-test-utils');
+
+// our component to be tested
+var TodoList = require('TodoList');
+
+describe('TodoList', () => {
+
+    it('should exist', () => {
+        expect(TodoList).toExist();
+    });
+
+});
