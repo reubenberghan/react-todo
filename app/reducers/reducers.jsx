ca799892e19a5e59a93d72fe48fdddd45833a04b
Reuben Berghan <reuben.berghan@gmail.com>
2016-10-17 07:00:27 +1100

    Add showCompleted and searchText reducers

---
+++ app/reducers/reducers.jsx
@@ -0,0 +1,19 @@
+'use strict';
+
+export const searchTextReducer = (state = '', action) => {
+    switch (action.type) {
+        case 'SET_SEARCH_TEXT':
+            return action.searchText;
+        default: 
+            return state;
+    };
+};
+
+export const showCompletedReducer = (state = false, action) => {
+    switch (action.type) {
+        case 'TOGGLE_SHOW_COMPLETED':
+            return !state;
+        default:
+            return state;
+    };
+};
