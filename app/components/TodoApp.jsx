2b536d106608ad1f335c8f9709c14f7ce743c4bc
Reuben Berghan <reuben.berghan@gmail.com>
2017-07-30 12:25:29 +1000

    Updated CSS class names

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -24,7 +24,7 @@ export const TodoApp = React.createClass({
                 </div>
                 <h1 className="page-title">Todo App</h1>
                 <div className="row">
-                    <div className="column small-centered small-11 medium-6 large-5">
+                    <div className="columns small-centered small-11 medium-6 large-5">
                         <div className="container">
                             <TodoSearch />
                             <TodoList />
